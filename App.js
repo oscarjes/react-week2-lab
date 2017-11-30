6f2a79f5aecff1c30fd9f1ea6875c043000fc3a0
thiendn <>
2017-11-30 20:26:11 +0700

    Complete milestone 2

--- App.js
+++ App.js
@@ -3,14 +3,45 @@ import { StyleSheet, Text, View } from 'react-native';
 import testData from './TEST_DATA.json';
 import TumblrList from './TumblrList';
 
+const api_key = "ty4XIgHVw2xYH8EiUrVjbYIaXqMlzXdz9cdc3H4sKp52Kk6fAv";
+const api_url = "https://api.tumblr.com/v2/blog/rickandmortythings/posts/photo";
+
 export default class App extends React.Component {
+
+  componentWillMount (){
+    this.setState ({
+      loading : true
+    }, 
+    () => {this.fetchWithPage(0)})
+  }
+
+  fetchWithPage(page){
+    fetch(`${api_url}?api_key=${api_key}&limit=4&offset=${page * 4}`)
+    .then ((data) => data.json())
+    .then ((json) => {
+      return new Promise(function(resolve, reject){
+        setTimeout(function() {
+          resolve(json)
+        }, 2000)
+      })
+    })
+    .then ((json) => {
+      this.setState({
+        posts : json.response.posts,
+        loading : false
+      })
+    })
+      
+    }
 
   constructor(props) {
     super(props);
   
     this.state = {
-      posts: testData.response.posts
+      posts: [],
+      loading : false,
     }
+    this.fetchWithPage = this.fetchWithPage.bind(this);
       
   }
 
